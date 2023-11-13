bfd6b914eaa3b8366bc3af5fe9599db0f97a212f
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-11-13 16:18:30 +0100

    Get weather data from weather api and log to console.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("test");
+async function getWeather() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=62db22a04a3148b1b9a104744231311&q=London&aqi=yes"
+  );
+  const weather = await response.json();
+  console.log(weather);
+}
+
+getWeather();
