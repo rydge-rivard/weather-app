e25645764dac31487a9029343c54bfa8c1ae950f
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-11-13 16:36:50 +0100

    Create weather obj factory and obj with required info after fetch.

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
-async function getWeather() {
+async function getWeather(location) {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=62db22a04a3148b1b9a104744231311&q=London&aqi=yes"
+    `https://api.weatherapi.com/v1/current.json?key=62db22a04a3148b1b9a104744231311&q=${location}&aqi=yes`
   );
   const weather = await response.json();
   console.log(weather);
+  const today = createWeatherObj(
+    weather.location.country,
+    weather.location.region,
+    weather.location.name,
+    weather.current.wind_dir,
+    weather.current.wind_kph,
+    weather.current.uv,
+    weather.current.temp_c,
+    weather.current.feelslike_c,
+    weather.current.condition.text,
+    weather.current.condition.icon
+  );
+  console.log(today);
 }
 
-getWeather();
+function createWeatherObj(
+  country,
+  region,
+  name,
+  wind_dir,
+  wind_kph,
+  uv,
+  temp_c,
+  feelslike_c,
+  text,
+  icon
+) {
+  return {
+    country,
+    region,
+    name,
+    wind_dir,
+    wind_kph,
+    uv,
+    temp_c,
+    feelslike_c,
+    text,
+    icon,
+  };
+}
+
+getWeather("Simcoe");
